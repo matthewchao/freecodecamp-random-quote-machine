9c86886f874eb2cc903ed887677ddfec46277e29
matthewchao <matthew.chao1@yahoo.com>
2020-10-16 21:48:30 -0400

    add random index property and click handler

--- random-quote-machine-simple/src/App.js
+++ random-quote-machine-simple/src/App.js
@@ -1,32 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 // my imports:
-import QuoteBox from './QuoteBox.js';
-import ChangeQuoteButton from './ChangeQuoteButton.js';
+import QuoteBox from "./QuoteBox.js";
+import ChangeQuoteButton from "./ChangeQuoteButton.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <QuoteBox />
-        <ChangeQuoteButton />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const quoteBank = [
+  { quote: "Hi", author: "mc" },
+  { quote: "hey", author: "jose" },
+  { quote: "herro" },
+];
+
+const n = quoteBank.length;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currIndex: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    console.log("clicked!");
+    let newIndex = null;
+    while (newIndex == null || newIndex == this.state.currIndex) {
+      newIndex = Math.floor(Math.random() * n);
+    }
+    this.setState({
+      currIndex: newIndex,
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div>will display quote with index: {this.state.currIndex}</div>
+          <QuoteBox />
+          <button onClick={this.handleClick}>Get a new quote</button>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;

fe1f8518b9e61c706e212db86997d66c908cee4d
matthewchao <matthew.chao1@yahoo.com>
2020-11-26 12:04:58 -0500

    remove create-react-app default elements

--- random-quote-machine-simple/src/App.js
+++ random-quote-machine-simple/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 
 // my imports:
@@ -50,7 +50,7 @@ class App extends React.Component {
           />
           {/* <button onClick={this.handleClick}>Get a new quote</button> */}
           <ChangeQuoteButton handleClick={this.handleClick} />
-          <img src={logo} className="App-logo" alt="logo" />
+          {/* <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
@@ -61,7 +61,7 @@ class App extends React.Component {
             rel="noopener noreferrer"
           >
             Learn React
-          </a>
+          </a> */}
         </header>
       </div>
     );
